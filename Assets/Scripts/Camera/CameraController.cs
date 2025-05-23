245017d0c01f95ee5c69d343a3e174230454accb
Yusuf Osman Avcı <yusufavci0516@gmail.com>
2025-05-23 20:49:02 +0300

    Controllers

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,14 +8,28 @@ public class CameraController : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    void LateUpdate()
+    {
+        if (targets == null || targets.Length == 0)
+        {
+            return;
+        }
+        Transform activeTarget = FindActiveTarget();
+        if (activeTarget == null)
+            return;
+
+        Vector3 desiredPosition = activeTarget.position + offset;
+        desiredPosition.y = transform.position.y;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
     Transform FindActiveTarget()
     {
-        foreach(Transform target in targets)
+        foreach (Transform target in targets)
         {
             if (target.gameObject.activeInHierarchy)
-            {
                 return target;
-            }
         }
         return null;
     }
--- Assets/Scripts/Character/NewControls/FightingController.cs
+++ Assets/Scripts/Character/NewControls/FightingController.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class FightingController : MonoBehaviour
 {
+    [Header("Player Movement")]
     public float movementSpeed = 1f;
     public float rotationSpeed = 10f;
-
     private CharacterController characterController;
     private Animator animator;
 
+    [Header("Player Fight")]
+    public float attackCooldown = 0.5f;
+    public int attackDamage = 5;
+    public string[] attackAnimations = { "Attack1Animation", "Attack2Animation", "Attack3Animation", "Attack4Animation" };
+    public float dodgeDistance = 2f;
+    private float lastAttackTime;
 
-    private void Awake()
+    [Header("Effects and sounds")]
+    public ParticleSystem attack1Effect;
+    public ParticleSystem attack2Effect;
+    public ParticleSystem attack3Effect;
+    public ParticleSystem attack4Effect;
+
+    void Awake()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
 
-    private void Update()
+    void Update()
     {
         PerformMovement();
+        PerformDodgeFront();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PerformAttack(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PerformAttack(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PerformAttack(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PerformAttack(3);
+        }
     }
 
     void PerformMovement()
@@ -27,22 +57,23 @@ public class FightingController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        Vector3 movement = new Vector3(-verticalInput, 0f, horizontalInput);
 
-        if(movement != Vector3.zero)
+        if (movement != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-            if(horizontalInput > 0)
+            if (horizontalInput > 0)
             {
                 animator.SetBool("Walking", true);
             }
-            else if(horizontalInput < 0)
+            else if (horizontalInput < 0)
             {
                 animator.SetBool("Walking", true);
             }
-            else if(verticalInput != 0)
+
+            else if (verticalInput != 0)
             {
                 animator.SetBool("Walking", true);
             }
@@ -52,6 +83,54 @@ public class FightingController : MonoBehaviour
             animator.SetBool("Walking", false);
         }
 
-            characterController.Move(movement * movementSpeed * Time.deltaTime);
+        characterController.Move(movement * movementSpeed * Time.deltaTime);
+
+    }
+
+    void PerformAttack(int attackIndex)
+    {
+        if (Time.time - lastAttackTime > attackCooldown)
+        {
+            animator.Play(attackAnimations[attackIndex]);
+
+            int damage = attackDamage;
+            Debug.Log("Performed attack" + (attackIndex + 1) + "dealing" + damage + "damage");
+
+            lastAttackTime = Time.time;
+
+            //Loop through each opponent
+        }
+        else
+        {
+            Debug.Log("Endlag");
+        }
+    }
+    void PerformDodgeFront()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.Play("DodgeFrontAnimation");
+
+            Vector3 dodgeDiretion = transform.forward * dodgeDistance;
+
+            characterController.Move(dodgeDiretion);
+        }
+    }
+
+    public void Attack1Effect()
+    {
+        attack1Effect.Play();
+    }
+    public void Attack2Effect()
+    {
+        attack2Effect.Play();
+    }
+    public void Attack3Effect()
+    {
+        attack3Effect.Play();
+    }
+    public void Attack4Effect()
+    {
+        attack4Effect.Play();
     }
 }
